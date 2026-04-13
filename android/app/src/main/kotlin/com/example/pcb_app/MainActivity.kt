70335156f1a1ddbcc3a3639ea7e405ac79d29e1a
appropose023-tech <appropose023@gmail.com>
2026-04-13 15:00:28 +0530

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/com/example/pcb_app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.pcb_aoi // 1. MUST match the package in your Manifest
+
+import io.flutter.embedding.android.FlutterActivity // 2. The "v2" import
+
+class MainActivity: FlutterActivity() {
+    // 3. Keep this body empty
+}
